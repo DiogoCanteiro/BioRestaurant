a419a944b1762c7ac07c0272e35c2f37375eeb3c
Diogo Canteiro <diogocanteiro@gmail.com>
2022-07-26 00:58:27 +0100

    wip (need to fix it)

---
+++ Bio.Services.ShoppingCartAPI.Data/Interfaces/ICartRepository.cs
@@ -0,0 +1,13 @@
+﻿using Bio.Services.ShoppingCartAPI.Data.DbModels;
+using System.Threading.Tasks;
+
+namespace Bio.Services.ShoppingCartAPI.Data.Interfaces
+{
+    public interface ICartRepository
+    {
+        Task<Cart> GetCartByUserId(string userId);
+        Task<Cart> CreateUpdateCart(Cart cart);
+        Task<bool> RemoveFromCart(int cardDetailsId);
+        Task<bool> ClearCart(string userId);
+    }
+}
---
+++ Bio.Services.ShoppingCartAPI.Data/Repositories/CartRepository.cs
@@ -0,0 +1,37 @@
+﻿using Bio.Services.ShoppingCartAPI.Data.DbContexts;
+using Bio.Services.ShoppingCartAPI.Data.DbModels;
+using Bio.Services.ShoppingCartAPI.Data.Interfaces;
+using System.Threading.Tasks;
+
+namespace Bio.Services.ShoppingCartAPI.Data.Repositories
+{
+    public class CartRepository : ICartRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CartRepository(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public async Task<bool> ClearCart(string userId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public async Task<Cart> CreateUpdateCart(Cart cart)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public async Task<Cart> GetCartByUserId(string userId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public async Task<bool> RemoveFromCart(int cardDetailsId)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
